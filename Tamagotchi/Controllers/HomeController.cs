633010f098b1f5e20c7e11d569c4afa552b5e93a
Elly Maimon and Matt Smith <elly@>
2018-07-05 13:38:05 -0700

    initial commit... sorta late

---
+++ Tamagotchi/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tamagotchi.Models;
+
+namespace Tamagotchi.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/new-critter")]
+        public ActionResult NewCritter()
+        {
+            return View();
+        }
+
+        [HttpGet("/critters")]
+        public ActionResult Critters()
+        {
+            List<Critter> allCritters = Critter.GetAll();
+            return View(allCritters);
+
+        }
+
+        [HttpPost("/critters")]
+        public ActionResult Create()
+        {
+            Critter newCritter = new Critter(Request.Form["new-critter"]);
+            List<Critter> allCritters = Critter.GetAll();
+            return View("Index", allCritters);
+        }
+
+        [HttpGet("/critters/{id}")]
+        public ActionResult CritterDetail(int id)
+        {
+            Critter critter = Critter.Find(id);
+            return View(critter);
+        }
+    }
+}
---
+++ Tamagotchi/Models/Critter.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Tamagotchi.Models
+{
+    public class Critter
+    {
+        private int _hunger = 100;
+        private int _attention = 100;
+        private int _rest = 100;
+        private int _id;
+        private string _name;
+        private static List<Critter> _critterList = new List<Critter>() { };
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public static Critter Find(int searchId)
+        {
+            return _critterList[searchId - 1];
+        }
+
+        public static List<Critter> GetAll()
+        {
+            return _critterList;
+        }
+
+        public Critter(string newName)
+        {
+            _name = newName;
+            _critterList.Add(this);
+            _id = _critterList.Count;
+        }
+
+        public int Find()
+        {
+            
+        }
+
+        public void Feed()
+        {
+            this._hunger += 20;
+            this._attention -= 1;
+            this._rest -= 1;
+        }
+
+        public void Play()
+        {
+            this._attention += 20;
+            this._hunger -= 1;
+            this._rest -= 1;
+        }
+
+        public void Rest()
+        {
+            this._rest += 20;
+            this._attention -= 1;
+            this._hunger -= 1;
+        }
+
+        public static void PassTime()
+        {
+            foreach (Critter Critter in _critterList)
+            {
+                Critter._hunger -= 5;
+                Critter._rest -= 5;
+                Critter._attention -= 5;
+            }
+
+        }
+    }
+}
